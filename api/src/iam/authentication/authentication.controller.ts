9b1ca857824ac0328953cd764bd526b745c573cd
Elayade Ismail <elayadeismail@gmail.com>
2024-06-29 21:18:43 +0100

    Add Sign In Endpoint

--- api/src/iam/authentication/authentication.controller.ts
+++ api/src/iam/authentication/authentication.controller.ts
@@ -1,6 +1,14 @@
-import { Body, Controller, Post, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Res,
+} from '@nestjs/common';
 import { Response } from 'express';
 import { AuthenticationService } from './authentication.service';
+import { SignInDto } from './dto/sign-in.dto';
 import { SignUpDto } from './dto/sign-up.dto';
 
 @Controller('authentication')
@@ -13,5 +21,14 @@ export class AuthenticationController {
     @Res({ passthrough: true }) response: Response,
   ) {
     return this.authenticationService.signUp(body, response);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Post('sign-in')
+  async signIn(
+    @Body() body: SignInDto,
+    @Res({ passthrough: true }) response: Response,
+  ) {
+    return this.authenticationService.signIn(body, response);
   }
 }
