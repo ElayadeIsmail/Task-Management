4a5f3745bab38698251e2feee1ca3c4a1498f7a6
Elayade Ismail <elayadeismail@gmail.com>
2024-06-29 21:02:15 +0100

    Register Jwt Module

--- api/src/iam/iam.module.ts
+++ api/src/iam/iam.module.ts
@@ -1,4 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { PrismaModule } from 'src/database/prisma.module';
+import { AuthenticationController } from './authentication/authentication.controller';
+import { AuthenticationService } from './authentication/authentication.service';
+import jwtConfig from './authentication/config/jwt.config';
 
-@Module({})
+@Module({
+  imports: [
+    JwtModule.registerAsync(jwtConfig.asProvider()),
+    ConfigModule.forFeature(jwtConfig),
+    PrismaModule,
+  ],
+  controllers: [AuthenticationController],
+  providers: [AuthenticationService],
+})
 export class IamModule {}
