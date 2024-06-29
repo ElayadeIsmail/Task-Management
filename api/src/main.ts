9f1a05d37e1aa8f5bd0cbb5919d136e37e528fc9
Elayade Ismail <elayadeismail@gmail.com>
2024-06-29 21:47:15 +0100

    Add compression and helmet middleware

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,12 +1,17 @@
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import compression from 'compression';
 import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get<ConfigService>(ConfigService);
+
+  app.use(compression());
+  app.use(helmet());
 
   // /api prefix
   app.setGlobalPrefix('/api');
