c82b9987bf507ef54472ea3e6f841a2be081fcf4
Elayade Ismail <elayadeismail@gmail.com>
2024-06-29 20:56:31 +0100

    Add jwt env validation

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import * as Joi from 'joi';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { IamModule } from './iam/iam.module';
 
 @Module({
   imports: [
@@ -12,9 +13,15 @@ import { AppService } from './app.service';
           .valid('development', 'production', 'test')
           .default('development'),
         DATABASE_URL: Joi.string(),
+        JWT_SECRET: Joi.string(),
+        JWT_TOKEN_AUDIENCE: Joi.string(),
+        JWT_TOKEN_ISSUER: Joi.string(),
+        JWT_ACCESS_TOKEN_TT: Joi.number().positive(),
+        JWT_REFRESH_TOKEN_TTL: Joi.number().positive(),
         PORT: Joi.number().port().default(8081),
       }),
     }),
+    IamModule,
   ],
   controllers: [AppController],
   providers: [AppService],
