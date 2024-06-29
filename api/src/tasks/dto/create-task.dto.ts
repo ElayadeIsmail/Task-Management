0b0ea8ea52662a8349c4fdcba5f7d79c6820afdd
Elayade Ismail <elayadeismail@gmail.com>
2024-06-29 21:41:59 +0100

    add create task TDO

--- api/src/tasks/dto/create-task.dto.ts
+++ api/src/tasks/dto/create-task.dto.ts
@@ -1,1 +1,16 @@
-export class CreateTaskDto {}
+import { TaskStatus } from '@prisma/client';
+import { IsEnum, IsOptional, IsString, MinLength } from 'class-validator';
+
+export class CreateTaskDto {
+  @IsString()
+  @MinLength(4)
+  title: string;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @IsEnum(TaskStatus)
+  @IsOptional()
+  status: TaskStatus;
+}
