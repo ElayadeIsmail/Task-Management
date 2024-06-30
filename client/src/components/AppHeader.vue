df58a3957a91c3f33d034bace6b058c068aa94f5
Elayade Ismail <elayadeismail@gmail.com>
2024-06-30 14:31:03 +0100

    Add AppHeader

---
+++ client/src/components/AppHeader.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+import { Button } from './ui/button'
+</script>
+
+<template>
+  <header class="container flex items-center justify-between h-12">
+    <RouterLink class="italic font-bold text-2xl" to="/">Task-Management</RouterLink>
+    <ul>
+      <li class="space-x-2">
+        <RouterLink to="/login">
+          <Button>Login</Button>
+        </RouterLink>
+        <RouterLink to="/register">
+          <Button variant="secondary">Register</Button>
+        </RouterLink>
+      </li>
+    </ul>
+  </header>
+</template>
