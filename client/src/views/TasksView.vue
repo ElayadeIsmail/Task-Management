797878589c340bcdd79f1a11e034b94f5d8aedb5
Elayade Ismail <elayadeismail@gmail.com>
2024-06-30 16:10:58 +0100

    change display for tasks page

--- client/src/views/TasksView.vue
+++ client/src/views/TasksView.vue
@@ -1,7 +1,30 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useQuery } from '@tanstack/vue-query'
+import * as tasksApi from '@/api/tasks.api'
+import * as tasksAdapter from '@/adapter/tasks.adapter'
+import type { TaskStatus } from '@/types'
+import { TaskColumn } from '@/components/tasks'
+
+const { isPending, isError, data, error } = useQuery({
+  queryKey: ['tasks'],
+  queryFn: () => tasksApi.getTasks().then((data) => tasksAdapter.adaptTasks(data || []))
+})
+</script>
 
 <template>
   <main class="container py-12">
-    <h2>Tasks</h2>
+    <div v-if="isPending">Loading...</div>
+    <div v-else-if="isError">An error has occurred: {{ error }}</div>
+    <div v-else-if="data">
+      <div class="grid grid-cols-3 justify-center gap-4 flex-col py-4">
+        <TaskColumn
+          v-for="[key, values] in Object.entries(data)"
+          :data="values"
+          :title="`${key} Tasks`"
+          :status="key as TaskStatus"
+          :key="key"
+        />
+      </div>
+    </div>
   </main>
 </template>
