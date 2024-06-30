dfd4edfd693ed9ab2d6cc2085057d47d84fefffb
Elayade Ismail <elayadeismail@gmail.com>
2024-06-30 16:10:42 +0100

    add tasks column

---
+++ client/src/components/tasks/TaskColumn.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { type ITask } from '@/types'
+import TaskCard from './TaskCard.vue'
+
+const props = defineProps<{
+  data: ITask[]
+  title: string
+  status: ITask['status']
+}>()
+</script>
+
+<template>
+  <div class="flex flex-col rounded-md border p-4 bg-neutral-50 h-fit">
+    <h2 class="text-center font-bold text-3xl text-neutral-600 mb-4">{{ props.title }}</h2>
+    <div class="flex flex-col space-y-3">
+      <TaskCard v-for="task in props.data" :task="task" :key="task.id" :status="props.status" />
+    </div>
+  </div>
+</template>
