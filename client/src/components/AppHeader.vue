c31dadb681b5f9c93290804ad9c003e4ce88b518
Elayade Ismail <elayadeismail@gmail.com>
2024-06-30 15:40:30 +0100

    change header actions depending on user

--- client/src/components/AppHeader.vue
+++ client/src/components/AppHeader.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
 import { RouterLink } from 'vue-router'
 import { Button } from './ui/button'
+import { useAuthStore } from '@/stores/auth.store'
+
+const authStore = useAuthStore()
 </script>
 
 <template>
@@ -8,12 +11,24 @@ import { Button } from './ui/button'
     <RouterLink class="italic font-bold text-2xl" to="/">Task-Management</RouterLink>
     <ul>
       <li class="space-x-2">
-        <RouterLink to="/login">
-          <Button>Login</Button>
-        </RouterLink>
-        <RouterLink to="/register">
-          <Button variant="secondary">Register</Button>
-        </RouterLink>
+        <!-- Auth Btn -->
+        <template v-if="authStore.user">
+          <RouterLink to="/tasks">
+            <Button variant="secondary">Tasks</Button>
+          </RouterLink>
+
+          <Button>Logout</Button>
+        </template>
+
+        <!-- Not Auth -->
+        <template v-else>
+          <RouterLink to="/login">
+            <Button>Login</Button>
+          </RouterLink>
+          <RouterLink to="/register">
+            <Button variant="secondary">Register</Button>
+          </RouterLink>
+        </template>
       </li>
     </ul>
   </header>
